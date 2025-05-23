492bc8696322339d913aa2452e2be559fa1b219b
Mo-way <139226735+Mo-way@users.noreply.github.com>
2025-05-23 22:24:11 +0000

    Write list of biggest values

--- clock_gettime/main.c
+++ clock_gettime/main.c
@@ -13,18 +13,33 @@ uint64_t getTimeLatencyNs() {
 }
 
 int main(void) {
-    // uint64_t runs = 1; // for -p function_graph
+    // uint64_t runs = 7; // for -p function_graph
     uint64_t runs = 1000000;
     uint64_t sum = 0;
-    uint64_t max = 0;
+    uint64_t max[9] = {0};
+    //uint64_t max = 0;
     for (int i = 0; i < runs; i++) {
         uint64_t delta = getTimeLatencyNs();
         // printf("%lu ", delta);
         sum += delta;
-        if (delta > max) {
-            max = delta;
+        int k_of_min = 0;
+        for (int k = 0; k < 9; k++) {
+            if (max[k_of_min] > max[k]) {
+                k_of_min = k;
+            }
         }
+        if (delta > max[k_of_min]) {
+            max[k_of_min] = delta;
+        }
+        //if (delta > max) {
+        //    max = delta;
+        //}
     }
-    printf("\n\nSum: %lu, Avg: %lu, Max: %lu\n", sum, sum / runs, max);
+    // printf("\n\nSum: %lu, Avg: %lu, Max: %lu\n", sum, sum / runs, max);
+    printf("\n\nSum: %lu, Avg: %lu\n", sum, sum / runs);
+    for (int i = 0; i < 9; i++) {
+        printf("%lu ", max[i]);
+    }
+    printf("\n");
     return 0;
 }
